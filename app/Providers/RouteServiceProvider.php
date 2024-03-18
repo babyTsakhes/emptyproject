d02719a049637f694c4dbcb63621cfae46535847
babyTsakhes <t48250klo@gmail.com>
2024-03-18 17:03:01 +0500

    добавили префикс api к пути

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -29,7 +29,7 @@ class RouteServiceProvider extends ServiceProvider
         });
 
         $this->routes(function () {
-            Route::middleware('api')
+            Route::middleware('api')->prefix('api')
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
